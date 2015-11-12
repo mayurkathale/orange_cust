1997af1e63b43d71c832c3a5f791d244957b0da9
mayuratsynerzip <mayurk@ULTP_375.synerzipune.local>
2015-11-12 09:05:26 +0530

    Adding created files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ohrmCustomizationApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ohrmCustomizationApp
+ */
+angular.module('ohrmCustomizationApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ohrmCustomizationApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ohrmCustomizationApp
+ */
+angular.module('ohrmCustomizationApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
