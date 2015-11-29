ed8383e6e0a63cc94511aaf5814261080cc92571
mayuratsynerzip <mayurk@ULTP_375.synerzipune.local>
2015-11-29 23:23:15 +0530

    update 3

---
+++ app/scripts/main.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $(document).click(function () {
+    var _opened = $("#js-navbar-collapse").hasClass("in");
+    if (_opened === true/* && !clickover.hasClass("navbar-toggle")*/) {
+      $("button.navbar-toggle").click();
+    }
+    return false;
+  });
+
+  function checkScroll(){
+    var startY = $('.navbar').height() * 2; //The point where the navbar changes in px
+
+    if($(window).scrollTop() > startY){
+      $('.navbar').addClass("scrolled");
+    }else{
+      $('.navbar').removeClass("scrolled");
+    }
+  }
+
+  if($('.navbar').length > 0){
+    $(window).on("scroll load resize", function(){
+      checkScroll();
+    });
+  }
+});
+
+tiles = $(".row.faded").fadeTo(0, 0);
+
+$(window).scroll(function(d,h) {
+  tiles.each(function(i) {
+    a = $(this).offset().top + $(this).height();
+    b = $(window).scrollTop() + $(window).height();
+    if (a < b) $(this).fadeTo(500,1);
+  });
+});
