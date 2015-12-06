41be8c0d9fc31d8f0532ef10f258d14ab46ef40d
mayuratsynerzip <mayurk@ULTP_375.synerzipune.local>
2015-12-07 02:28:20 +0530

    fix in send mail

--- app/php/sendContactEmail.php
+++ app/php/sendContactEmail.php
@@ -5,7 +5,7 @@
   $message = $_POST['message'];
   $name = $_POST['name'];
   $admin_email = "mayur.kathale@gmail.com";
-  $from = "mayur.mbll@gmail.com"
+  $from = "mayur.mbll@gmail.com";
 
   //send email
   mail($admin_email, $subject, $message." from ".$name." (".$email.")");
